cc70ba6350f91242d2ce083519ca3445d375e057
Lee Yeong Khang <yeong-khang.lee@vitrox.com>
2016-12-20 17:48:57 +0800

    automatic adjust height base on window height

--- src/app/data/data.component.ts
+++ src/app/data/data.component.ts
@@ -1,4 +1,4 @@
-import { Component, Optional } from '@angular/core';
+import { Component, Optional, ElementRef, ViewChild, HostListener } from '@angular/core';
 import { MdDialog, MdDialogRef } from '@angular/material';
 import { Http, RequestOptionsArgs, Headers } from '@angular/http';
 import { AppState } from '../app.service';
@@ -12,10 +12,11 @@ import { DataService } from './data.service';
 })
 export class DataComponent {
     private datas: Array<any> = []
-    private stateListener: any;
-    private sub: any;
     private id: any;
-
+    private sub: any;
+    @ViewChild('stage') stage: ElementRef;
+    private height: number = 0;
+    
     constructor(private _state: AppState,
                 private route: ActivatedRoute,
                 private router: Router,
@@ -24,10 +25,23 @@ export class DataComponent {
     ngOnInit(){
         this.sub = this.route.params.subscribe(params => {
             this.id = +params['id'];
-            console.log(this.id)
             this._data.getData(this.id).subscribe((v: any)=>{
-                console.log(v);
+                this.datas = v['data'];
             })
         });
+    }
+
+    ngAfterViewInit(){
+        this._updateHeight();
+    }
+
+    @HostListener('window:resize', ['$event'])
+    private _updateHeight(){
+        let dim = this.stage.nativeElement.getBoundingClientRect()
+        this.height = window.innerHeight - dim.top;
+    }
+
+    ngOnDestroy(){
+        this.sub.unsubscribe();
     }
 }
